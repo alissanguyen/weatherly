f268c97305ae5edc79c2f64d7c7cecc04b324e19
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2020-11-06 19:19:19 +0100

    Add API key, add form to get weather based on city name.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { API_KEY } from "./constants";
+import React from "react";
 
 /**
  * BASIC FEATURES
@@ -9,21 +10,43 @@ import './App.css';
  * 3. Choose and display location and date
  */
 
- /**
-  * ADVANCED FEATURES
-  * 1. Users can click on a specific day to see the hourly forecast
-  * 2. Add graphic library like 'vx'
-  */
- 
+/**
+ * ADVANCED FEATURES
+ * 1. Users can click on a specific day to see the hourly forecast
+ * 2. Add graphic library like 'vx'
+ */
+
+const GetWeatherForm = (props) => {
+  const [city, setCity] = React.useState("seattle");
+
+  return (
+    <form onSubmit={(e) => e.preventDefault()}>
+      <label htmlFor="city-input"> City </label>
+      <input
+        id="city-input"
+        onChange={(e) => setCity(e.target.value)}
+        placeholder="Type your city name..."
+      />
+      <button onClick={() => props.getWeather(city)}>Get Weather</button>
+    </form>
+  );
+};
+
 function App() {
+  const getWeather = (city) => {
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`)
+      .then((res) => {
+        console.log(res.json());
+      })
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Blue Sky
-        </h1>
+        <h1>Blue Sky</h1>
       </header>
+      <GetWeatherForm getWeather={(city) => getWeather(city)} />
     </div>
   );
 }
