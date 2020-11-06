11cd31cea3859ac5e728197c9770349a83d4323f
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2020-11-05 23:48:50 -0800

    Outline app features, add icons.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+/**
+ * BASIC FEATURES
+ * 1. Refresh every 5-10 minutes with the exact temp and weather conditions
+ * 2. Should have responsive designs
+ * 3. Choose and display location and date
+ */
+
+ /**
+  * ADVANCED FEATURES
+  * 1. Users can click on a specific day to see the hourly forecast
+  * 2. Add graphic library like 'vx'
+  */
+ 
 function App() {
   return (
     <div className="App">
---
+++ src/components/WeatherCard.js
@@ -0,0 +1,7 @@
+/**
+ * 1. Display the icon of the weather (rain, sunny, cloudy, lightnings, snowy, etc.)
+ * 2. Display the temperature (Celsius, Farenheit) --> Both high and low temp of each day
+ * 3. Display the humidity and wind speed
+ * 4. (Extension) background gif for the weather condition 
+ * 
+ */
