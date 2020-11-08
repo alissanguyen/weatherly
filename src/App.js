bb45b39e85c0da6a539020ede1f25a71608f5e25
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2020-11-08 00:00:17 -0800

    Add unit button

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 import { API_KEY } from "./constants";
 import React from "react";
+import WeatherCard from "./components/WeatherCard";
 
 /**
  * BASIC FEATURES
  * 1. Refresh every 5-10 minutes with the exact temp and weather conditions
  * 2. Should have responsive designs
  * 3. Choose and display location and date
+ * 4. Can switch between Farenheit and Celsius
+ * 5. Can switch language
  */
 
 /**
@@ -17,37 +20,69 @@ import React from "react";
  */
 
 const GetWeatherForm = (props) => {
-  const [city, setCity] = React.useState("seattle");
+  const [city, setCity] = React.useState(null);
 
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        props.getWeather(city);
+      }}
+    >
       <label htmlFor="city-input"> City </label>
       <input
+        required
         id="city-input"
         onChange={(e) => setCity(e.target.value)}
         placeholder="Type your city name..."
       />
-      <button onClick={() => props.getWeather(city)}>Get Weather</button>
+      <button type="submit">Get Weather</button>
     </form>
   );
 };
 
 function App() {
+  const [weatherData, setWeatherData] = React.useState(null);
+  const [unit, setUnit] = React.useState('Celsius')
+
   const getWeather = (city) => {
-    fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`)
+    fetch(
+      `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`
+    )
       .then((res) => {
-        console.log(res.json());
+        return res.json();
       })
+      .then((json) => {
+        const simplifiedData = {
+          city: json.city.name,
+          country: json.city.country,
+          data: json.list.map((a, index) => {
+            const newDate = new Date(a.dt_txt);
+            return {
+              ...a,
+              index: index,
+              date: newDate.toDateString(),
+              time: newDate.toTimeString(),
+            };
+          }),
+        };
+
+        setWeatherData(simplifiedData);
+      });
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Blue Sky</h1>
-      </header>
-      <GetWeatherForm getWeather={(city) => getWeather(city)} />
-    </div>
+    <React.Fragment>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Blue Sky</h1>
+        </header>
+  <button id="unit-button" onClick={() => setUnit('Farenheit')}>{unit}</button>
+        <GetWeatherForm getWeather={(city) => getWeather(city)} />
+      </div>
+      {weatherData ? <WeatherCard weather={weatherData} /> : null}
+    </React.Fragment>
   );
 }
 
--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -2,6 +2,59 @@
  * 1. Display the icon of the weather (rain, sunny, cloudy, lightnings, snowy, etc.)
  * 2. Display the temperature (Celsius, Farenheit) --> Both high and low temp of each day
  * 3. Display the humidity and wind speed
- * 4. (Extension) background gif for the weather condition 
- * 
+ * 4. (Extension) background gif for the weather condition
+ *
  */
+
+import React from "react";
+
+const WeatherCard = (props) => {
+  const data = props.weather;
+
+  function groupBy(array, property) {
+    return array.reduce(function (memo, x) {
+      if (!memo[x[property]]) {
+        memo[x[property]] = [];
+      }
+      memo[x[property]].push(x);
+      return memo;
+    }, {});
+  }
+
+  function convertKelvinToCelsius(temperature) {
+      const cTemperature = temperature - 273;
+  }
+
+  function convertKelvinToFarenheit(temperature) {
+      const fTemperature = (( (temperature - 273.15) * 9 ) / 5 ) + 32
+  }
+
+  let sortedData = groupBy(data.data, "date");
+  console.log(sortedData);
+
+  /**
+   * (for i=0; i < sortedData.size(); i++) { return ()}
+   * 
+   * 
+   * 
+   * sortedData[i].date //date
+   * sortedData[i].time //time
+   * sortedData[i].main.humidity
+   * sortedData[i].main.temp
+   * sortedData[i].main.temp_max
+   * sortedData[i].main.temp_min
+   * sortedData[i].weather[0].description
+   * 
+   * 
+   */
+  
+  return (
+    <React.Fragment>
+      <div className="weather-card">
+        <h4>{data.city}</h4>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default WeatherCard;
